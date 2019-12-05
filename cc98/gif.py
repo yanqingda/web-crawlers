b23ad0c55afba3037e0ea55ceb24a1cadf4e44fe
yanqingda <21729006@zju.edu.cn>
2019-12-05 12:46:24 +0800

    Update gif.py

--- cc98/gif.py
+++ cc98/gif.py
@@ -13,14 +13,15 @@ data = {
     'client_id': '9a1fd200-8687-44b1-4c20-08d50a96e5cd',
     'client_secret': '8b53f727-08e2-4509-8857-e34bf92b27f2',
     'grant_type': 'password',
-    'username': username,
-    'password': password,
+    'username':斋藤飞鸟,
+    'password': yanqingda,
 }
 
 token_url = 'https://openid.cc98.org/connect/token'
 resp = requests.post(url=token_url, data=data, headers=headers).json()
 headers['authorization'] = '{} {}'.format(resp['token_type'], resp['access_token'])
 headers['content-type'] = 'application/json'
-gif_url = 'https://api-v2.cc98.org/me/portrait'
+gif_url = '[img]http://file.cc98.org/v2-upload/txw1oaia.gif[/img]
+'
 resp = requests.put(url=gif_url, data=bytes('"{}"'.format(image), encoding='utf-8'), headers=headers)
 print('ok' if resp.status_code == 200 else 'fail')
